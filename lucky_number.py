0c9b47c88963cd0ae640614d34a2408e627c02e5
buyongji <buyongji@viabtc.com>
2019-04-24 20:04:49 +0800

    lucky number

---
+++ lucky_number.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import sys
+import random
+
+
+def main(seeds, start_num, end_num, count):
+    results = set()
+    random.seed(seeds)
+    while len(results) < count:
+        r = random.randint(start_num, end_num)
+        results.add(r)
+
+    for item in sorted(results):
+        print item
+
+
+if __name__ == '__main__':
+    """
+    python lucky_number.py "0000000000000000000b99fea8fac0fac12f045a95a07736f639a51dd70c47ff" 1000 10000 10
+    """
+    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
